5cc71fd2f74b711e9b983ec54655b98fbd6630a3
maxkhvorov <maxmang@gmail.com>
2015-02-09 11:49:17 +0300

    Add fabric

---
+++ fabfile.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+
+from fabric.api import local, run, put, sudo, env
+
+def replace(filename, f, t):
+    text = file(filename, 'rb').read().replace(f, t)
+    file(filename, 'wb').write(text)
+
+
+def build_sdist(version):
+    local('rm -rf ./build-sdist')
+    local('mkdir -p ./build-sdist/webdriver/webdriver')
+    local('cp -R ./webdriver/* ./build-sdist/webdriver/webdriver/')
+    local('cp ./setup.py ./build-sdist/webdriver/')
+    replace('./build-sdist/webdriver/setup.py', 'version="0"', 'version="' + version + '"')
+    replace('./build-sdist/webdriver/webdriver/__init__.py', 'version = "0"', 'version = "' + version + '"')
+    replace('./build-sdist/webdriver/webdriver/__init__.py', 'version_info = (0,)', 'version_info = "' + ', '.join([str(int(x)) for x in version.split('.')]) + '"')
+    local('cd ./build-sdist/webdriver/;python setup.py sdist')
+    return './build-sdist/webdriver/dist/webdriver-' + version + '.tar.gz'
+
+
+def test():
+    env.host_string = '172.20.20.248'
+    env.user = 'vagrant'
+    env.password = 'vagrant'
+    version = time.strftime('%Y.%m.%d.%H.%M.%S', time.localtime())
+    filename = build_sdist(version)
+    run('rm -rf /tmp/webdriver;mkdir /tmp/webdriver')
+    put(filename, '/tmp/webdriver/webdriver.tar.gz')
+    local('rm -rf ./build-sdist')
+    run('cd /tmp/webdriver;tar zxf ./webdriver.tar.gz')
+    sudo('cd /tmp/webdriver/webdriver-' + version + '/;python setup.py install;rm -rf /tmp/webdriver')
+
+
+
+def release():
+    pass
+
+
+def main():
+    if sys.argv[1] == 'test':
+        test()
+    elif sys.argv[1] == 'release':
+        release()
+
+
+if __name__ == '__main__':
+    main()
